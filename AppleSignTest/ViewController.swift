15a7d2abb4ba01408cc418cee37e18bab478190d
Ignacio Acisclo Pérez <iacisclo@autentia.com>
2020-05-18 13:04:26 +0200

    Initial Commit

---
+++ AppleSignTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleSignTest
+//
+//  Created by Ignacio Acisclo on 18/05/2020.
+//  Copyright © 2020 iAcisclo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
